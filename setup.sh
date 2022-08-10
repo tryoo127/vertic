753ca691f2a1adc54676ff468b599a6f142978bc
tryoo127 <tryoo127@gmail.com>
2022-08-10 17:09:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip)
+red='\e[1;31m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl -sS https://raw.githubusercontent.com/tryoo127/access/main/ip | awk '{print $4}' | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
-echo -e "\e[0;32m            PERMISSION ACCEPTED!\e[0m"
+clear
+echo -e "${green}            PERMISSION ACCEPTED!${NC}"
 sleep 3
 else
+clear
+echo ""
 rm -f setup.sh
-echo -e "\e[1;31m              PERMISSION DENIED!\e[0m"
-echo -e "\e[1;31m PLEASE CONTACT @XoolVPN TO REGISTER YOUR I.P\e[0m"
-sleep 5
+echo -e "${red}              PERMISSION DENIED!${NC}"
+echo -e "${green} PLEASE CONTACT @XoolVPN TO REGISTER YOUR I.P${NC}"
+sleep 7
 exit 0
 fi
 clear
-
 #Welcome Note
 echo -e "============================================="
 echo -e "     AUTOSCRIPT-FULL (V1.0) BY @XoolVPN "
